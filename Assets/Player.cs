c4f9de8933387988c81d13a09a263324d1d894d5
SriramVenkatP <211501105@rajalakshmi.edu.in>
2024-01-01 01:40:13 +0530

    first commit
    commit

---
+++ Assets/Player.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    public gameOverScreen GameOverScreen;
+    public Camera playerCamera;
+    public float walkSpeed = 6f;
+    public float runSpeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+    public float defaultHeight = 2f;
+    public float crouchHeight = 1f;
+    public float crouchSpeed = 3f;
+    public TextMeshProUGUI pointsText;
+    int maxPlatform=0;
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+    private CharacterController characterController;
+
+    private bool canMove = true;
+
+    public Animator playerAnimator;
+    public Transform playerTrans;
+    public int points;
+
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float RemainingTime;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpPower;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.R) && canMove)
+        {
+            characterController.height = crouchHeight;
+            walkSpeed = crouchSpeed;
+            runSpeed = crouchSpeed;
+
+            // Additional: Add crouch animation logic here
+        }
+        else
+        {
+            characterController.height = defaultHeight;
+            walkSpeed = 6f;
+            runSpeed = 12f;
+
+            // Additional: Add stand up animation logic here
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+
+        // Animation logic
+        if (curSpeedX != 0 || curSpeedY != 0)
+        {
+            playerAnimator.SetBool("Running", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("Running", false);
+            playerAnimator.SetTrigger("Idle");
+        }
+
+        if(RemainingTime>0)
+        {
+            RemainingTime -= Time.deltaTime;
+        }
+        else if(RemainingTime<1)
+        {
+            RemainingTime = 0;
+            timerText.color = Color.red;
+        }
+        
+        int minutes= Mathf.FloorToInt(RemainingTime / 60);
+        int seconds = Mathf.FloorToInt(RemainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Point"))
+        {
+            Debug.Log("Point collected!");
+            points++;
+            Destroy(col.gameObject);
+            pointsText.text = "Points: "+ points.ToString();
+        }
+    }
+
+    public void GameOver(){
+        GameOverScreen.Setup(maxPlatform);
+    } 
+}
